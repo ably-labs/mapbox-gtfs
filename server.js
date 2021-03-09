47c92e2874fdcb646228795e64c6c0673b13a82b
tomC <tc13270@my.bristol.ac.uk>
2021-03-09 16:00:56 +0000

    Add server

--- server.js
+++ server.js
@@ -1,14 +1,53 @@
-const express = require('express');
-const app = express();
+const Ably = require("ably");
 const path = require('path');
-const router = express.Router();
+require('dotenv').config();
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
+const ApiKey = process.env.ABLY_API_KEY; /* Add your API key here */
+
+/* Instance the Ably REST server library */
+var rest = new Ably.Rest({ key: ApiKey });
+
+/* Start the Express.js web server */
+const express = require('express'),
+      app = express(),
+      cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+
+/* Server static content from the root path to keep things simple */
+app.use('/', express.static(__dirname));
+
+/* Issue token requests to clients sending a request
+   to the /auth endpoint */
+app.get('/auth', function (req, res) {
+  var tokenParams;
+  /* Check if the user is logged in */
+    tokenParams = {
+      'capability': { '[product:ably-transport/vehicles]*': ['subscribe'] }
+    };
+
+  console.log("Sending signed token request:", JSON.stringify(tokenParams));
+  rest.auth.createTokenRequest(tokenParams, function(err, tokenRequest) {
+    if (err) {
+      res.status(500).send('Error requesting token: ' + JSON.stringify(err));
+    } else {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(tokenRequest));
+    }
+  });
 });
 
-//add the router
-app.use('/', router);
-app.listen(process.env.port || 3000);
+app.get('/',function(req,res){
+    console.log(path);
+    console.log("BEEP");
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
 
-console.log('Running at Port 3000');
+app.get('/lines',function(req,res){
+    console.log(__dirname+'/lines.html');
+    res.sendFile(path.join(__dirname+'/lines.html'));
+});
+
+app.listen(3000, function () {
+    console.log('Web server listening on port 3000');
+});
